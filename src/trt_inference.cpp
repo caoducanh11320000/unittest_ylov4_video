4e37775c174f560943a3bb7962bf057957a14de6
Tuannt <tuannt1531@gmail.com>
2023-11-28 11:12:55 +0700

    init repo

---
+++ src/trt_inference.cpp
@@ -0,0 +1,17 @@
+#include "trt_inference.hpp"
+
+using namespace IMXAIEngine;
+
+TRT_Inference::TRT_Inference(){
+
+}
+
+trt_error TRT_Inference::init_inference(int argc, char **argv){
+
+
+}
+
+
+trt_error TRT_Inference::trt_detection(std::vector<cv::Mat> &input_img, std::vector<trt_results> &results){
+
+}
